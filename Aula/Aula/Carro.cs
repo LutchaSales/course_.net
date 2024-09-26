95366ca62e4aee5760ca0756d600d5338227de47
Luciano de Jesus Sales <123966631+LutchaSales@users.noreply.github.com>
2024-09-25 21:33:52 -0300

    Ok ok
    Estudo dia 25/09/2024

---
+++ Aula/Aula/Carro.cs
@@ -0,0 +1,52 @@
+﻿
+
+namespace Aula
+{
+    internal class Carro 
+    {
+        public Carro (string marca, string modelo, double cavalos, double peso) 
+        { 
+            Marca = marca;
+            Modelo = modelo;
+            Cavalos = cavalos;
+            Peso = peso;
+            Potencia = CalcPesoPotencia();
+        }
+
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public double Cavalos { get; set; }
+        public double Peso { get; set; }
+
+        public double Potencia { get; }
+
+        private double CalcPesoPotencia()
+        {
+            return Peso / Cavalos;
+        }
+
+        public static void MostrarInfo(Carro carro)
+        {
+            Console.WriteLine($"Marca: {carro.Marca}, \n" +
+                            $"Modelo: {carro.Modelo}, \n" +
+                            $"Cavalos: {carro.Cavalos}, \n" +
+                            $"Peso: {carro.Peso}, \n" +
+                            $"A Potência do carro é: {(int)carro.Potencia}");
+            Console.WriteLine();
+        }
+
+        public void MostreInfo()
+        {
+            Console.WriteLine($"Marca: {this.Marca}, \n" +
+                            $"Modelo: {this.Modelo}, \n" +
+                            $"Cavalos: {this.Cavalos}, \n" +
+                            $"Peso: {this.Peso}, \n" +
+                            $"A Potência do carro é: {(int)this.Potencia}");
+            Console.WriteLine();
+        }
+
+        
+
+    }
+
+}
---
+++ Aula/Aula/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Aula
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro1 = new Carro("Fiat", "Argo", 98, 1350);
+
+            Carro carro2 = new Carro("Hyundai", "Creta", 135, 1520);
+
+
+            Carro.MostrarInfo(carro1);
+
+            carro2.MostreInfo();
+        }
+    }
+}
